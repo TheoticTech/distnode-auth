448149125e5fcba196cefbb30d26ccbfccab34fb
GregCKrause <gregckrause@gmail.com>
2022-03-02 22:28:12 -0600

    Revert "Adding response header Vary: Origin"
    This reverts commit 5e9fc2f49343daeaf99c63a38b9ce00eea9a2c50.

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -9,7 +9,6 @@ const corsMiddleware = (req, res, next): express.Response => {
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Set-Cookie')
-  res.header('Vary', 'Origin')
   return next()
 }
 
